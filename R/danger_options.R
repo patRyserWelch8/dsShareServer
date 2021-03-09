51de06093311ff220f21aac836d0048bf0c1c8de
redraven <patricia.ryser-welch@newcastle.ac.uk>
2021-03-09 17:30:13 +0000

    danger_options

---
+++ R/danger_options.R
@@ -0,0 +1,9 @@
+#'@name danger_options
+#'@title retrieve the settings for the package
+#'@description This function uses the option "dsSS_settings" and the global enviroment
+#'to retrieve the settings
+#'@export
+danger_options <- function()
+{
+  return(.Options$dsSS_sharing.allowed)
+}
